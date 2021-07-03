240631f515d788747edfb557b0110037e6cb3927
Hannes Achleitner <hannes.software@gmx.at>
2021-07-03 16:12:47 +0200

    Rebranding

--- mail/common/src/main/java/com/fsck/k9/mail/filter/Hex.kt
+++ mail/common/src/main/java/com/fsck/k9/mail/filter/Hex.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 The K-9 Dog Walkers
+ * Copyright 2018 The L-10 Dog Walkers
  * Copyright 2001-2004 The Apache Software Foundation.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
